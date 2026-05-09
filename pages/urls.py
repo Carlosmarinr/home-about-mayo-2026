71aa6c9e92312093064f87000e4127978e7a6155
carlosmarinr <carlosmarin2511@gmail.com>
2026-05-09 00:35:07 -0400

    about

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from.views import HomeView
+from.views import AboutView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name ='')
+    path('', HomeView.as_view(), name =''),
+    path('', AboutView.as_view(), name ='')
     
 ]
